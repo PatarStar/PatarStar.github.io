6102adb31e948e99762dd1226070680241ddd44c
PatarStar <89463058+PatarStar@users.noreply.github.com>
2023-03-31 01:22:39 +0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,127 @@
+$(function () {
+    $(".penguin").on('click', function(){
+        alert("呼拉屁股啦！");
+    });
+
+    $(".Anpanman").on('click', function(){
+        alert("麵包超人啦！");
+    });
+
+    $(".cake").on('click', function(){
+        alert("草莓蛋糕屁股啦！");
+    });
+
+    $("#1").on('click',  function(){
+        $("#t1").toggle();
+    });
+
+    $("#2").on('click',  function(){
+        $("#t2").toggle();
+    });
+
+    $("#3").on('click',  function(){
+        $("#t3").toggle();
+    });
+
+    $("#4").on('click',  function(){
+        $("#t4").toggle();
+    });
+
+    $("#5").on('click',  function(){
+        $("#t5").toggle();
+    });
+
+    $("#6").on('click',  function(){
+        $("#t6").toggle();
+    });
+
+    $("#7").on('click',  function(){
+        $("#t7").toggle();
+    });
+
+    $("#8").on('click',  function(){
+        $("#t8").toggle();
+    });
+
+    $("#9").on('click',  function(){
+        $("#t9").toggle();
+    });
+
+    $("#10").on('click',  function(){
+        $("#t10").toggle();
+    });
+
+    $("#11").on('click',  function(){
+        $("#t11").toggle();
+    });
+
+    $("#12").on('click',  function(){
+        $("#t12").toggle();
+    });
+
+    $("#13").on('click',  function(){
+        $("#t13").toggle();
+    });
+
+    $("#14").on('click',  function(){
+        $("#t14").toggle();
+    });
+
+    $("#15").on('click',  function(){
+        $("#t15").toggle();
+    });
+
+    $("#16").on('click',  function(){
+        $("#t16").toggle();
+    });
+
+    $("#17").on('click',  function(){
+        $("#t17").toggle();
+    });
+
+    $("#18").on('click',  function(){
+        $("#t18").toggle();
+    });
+
+    $("#19").on('click',  function(){
+        $("#t19").toggle();
+    });
+
+    $("#20").on('click',  function(){
+        $("#t20").toggle();
+    });
+
+    $("#21").on('click',  function(){
+        $("#t21").toggle();
+    });
+
+    $("#22").on('click',  function(){
+        $("#t22").toggle();
+    });
+
+    $("#23").on('click',  function(){
+        $("#t23").toggle();
+    });
+
+    $("#24").on('click',  function(){
+        $("#t24").toggle();
+    });
+
+    $("#25").on('click',  function(){
+        $("#t25").toggle();
+    });
+
+    $("#26").on('click',  function(){
+        $("#t26").toggle();
+    });
+
+    $("#27").on('click',  function(){
+        $("#t27").toggle();
+        $(".birthday").toggle();
+    });
+
+    $("#t27").on('click',  function(){
+        $("#t27").toggle();
+        $(".birthday").toggle();
+    });
+});
